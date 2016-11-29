046d1084eca43d68fafa07dc17265a502e4d0270
Azonic89 <ooze89@yahoo.com>
2016-11-29 18:32:14 +0200

    Refactored

--- config/passport.js
+++ config/passport.js
@@ -10,18 +10,22 @@ module.exports = function({ app, data }) {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    passport.use(new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
         User.findOne({ email: email.toLowerCase() }, (err, user) => {
-            if (err) { return done(err); }
+            if (err) {
+                return done(err);
+            }
             if (!user) {
                 return done(null, false, { msg: `Email ${email} not found.` });
             }
             user.comparePassword(password, (err, isMatch) => {
-                if (err) { return done(err); }
+                if (err) {
+                    return done(err);
+                }
                 if (isMatch) {
                     return done(null, user);
                 }
-                return done(null, false, { msg: 'Invalid email or password.' });
+                return done(null, false, { msg: "Invalid email or password." });
             });
         });
     }));
@@ -90,8 +94,6 @@ module.exports = function({ app, data }) {
             });
         }
     }));
-
-    // passport.use(strategy);
 
     passport.serializeUser((user, done) => {
         if (user) {
