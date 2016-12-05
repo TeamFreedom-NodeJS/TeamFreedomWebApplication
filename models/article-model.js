a258b9c68ac58c01ed7b8237bbc66ebcbc0a35ef
dushka-dragoeva <d.dragoeva@abv.bg>
2016-12-05 12:00:52 +0200

    small fixes

--- models/article-model.js
+++ models/article-model.js
@@ -5,13 +5,11 @@ const modelRegistrator = require("./utils/model-registrator");
 module.exports = modelRegistrator.register("Article", {
     title: {
         type: String,
-        validate: /[а-яА-Я ]+\w /,
         required: true,
         unique: true
     },
     imgUrl: {
         type: String,
-        validate: /^(ftp|http|https):\/\/[^ "]+$/,
         required: true
     },
     content: {
